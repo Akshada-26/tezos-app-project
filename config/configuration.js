6d3d32ad2749ae9e6ab44ce8303842fb0b4d6a82
Anil <anil@b9lab.com>
2021-07-19 14:20:52 +0200

    rename field

--- config/configuration.js
+++ config/configuration.js
@@ -1,7 +1,7 @@
 exports.configuration = () => {
     return {
         "contractAddress": "KT1WFEN49LJtCK3msiNukaDQiV5i3c9BGqLZ",
-        "privateKey": "tz1hRTppkUow3wQNcj9nZ9s5snwc6sGC8QHh",
+        "orgAccount": "tz1hRTppkUow3wQNcj9nZ9s5snwc6sGC8QHh",
         "chain": "florencenet",
         "apiEndpoint": "https://api.florencenet.tzkt.io/",
         "provider": "https://florencenet.smartpy.io/"

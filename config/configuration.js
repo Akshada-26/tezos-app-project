1f51b1716f3404cd4f429aaca359c9b3641bf630
Anil <anil@b9lab.com>
2021-08-20 17:32:26 +0200

    adjust contract address

--- config/configuration.js
+++ config/configuration.js
@@ -1,6 +1,6 @@
 exports.configuration = () => {
     return {
-        "contractAddress": "KT1JcN4UYJaayw4iaBZUgBPS2CCYdg3aLLC8",
+        "contractAddress": "KT1Ji1JQt2rfFTPAgaK9nFwA3s23uu63kTRX",
         "orgAccount": "tz1hRTppkUow3wQNcj9nZ9s5snwc6sGC8QHh",
         "chain": "florencenet",
         "apiEndpoint": "https://api.florencenet.tzkt.io/",

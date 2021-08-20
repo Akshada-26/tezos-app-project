b4eb11e0ff5e3ce39b6914dbf058a7792e03bed8
Ibo Sy <ibo@b9lab.com>
2021-08-20 13:38:40 +0200

    revert configuration to public testnet

--- config/configuration.js
+++ config/configuration.js
@@ -1,9 +1,9 @@
 exports.configuration = () => {
     return {
-        "contractAddress": "KT1LhEe4rZKLmJUZKqQ2E4z6MRGAnjUdJdyV",
+        "contractAddress": "KT1JcN4UYJaayw4iaBZUgBPS2CCYdg3aLLC8",
         "orgAccount": "tz1hRTppkUow3wQNcj9nZ9s5snwc6sGC8QHh",
-        "chain": "custom",
+        "chain": "florencenet",
         "apiEndpoint": "https://api.florencenet.tzkt.io/",
-        "provider": "http://localhost:20000"
+        "provider": "https://florencenet.smartpy.io/"
     };
 };

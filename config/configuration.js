8966a91929abb6311a376802f8d2972192bc61c2
Gioele Bonifetto <gioele@b9lab.com>
2021-08-26 18:13:41 +0200

    updates config file with env values

--- config/configuration.js
+++ config/configuration.js
@@ -1,9 +1,9 @@
 exports.configuration = () => {
     return {
-        "contractAddress": "KT1GHRd14xrbUDDGPR2G1PFwUSpEEwFPDnSX",
+        "contractAddress": process.env.CONTRACT_ADDRESS || "KT1GHRd14xrbUDDGPR2G1PFwUSpEEwFPDnSX",
         "orgAccount": "tz1hRTppkUow3wQNcj9nZ9s5snwc6sGC8QHh",
-        "chain": "florencenet",
-        "apiEndpoint": "https://api.florencenet.tzkt.io/",
-        "provider": "https://florencenet.smartpy.io/"
+        "chain": process.env.CHAIN || "florencenet",
+        "apiEndpoint": process.env.INDEXER_API_ENDPOINT || "https://api.florencenet.tzkt.io/",
+        "provider": process.env.NODE_PROVIDER || "https://florencenet.smartpy.io/"
     };
 };

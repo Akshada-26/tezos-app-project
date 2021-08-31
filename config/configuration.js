28b5a716ef94dfdfd3ff71a553c1a102ecc635ab
Gioele Bonifetto <gioele@b9lab.com>
2021-08-31 16:04:53 +0200

    updates env names

--- config/configuration.js
+++ config/configuration.js
@@ -1,9 +1,11 @@
+// require('dotenv').config();
+
 exports.configuration = () => {
     return {
-        "contractAddress": process.env.CONTRACT_ADDRESS || "KT1GHRd14xrbUDDGPR2G1PFwUSpEEwFPDnSX",
+        "contractAddress": process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || "KT1GHRd14xrbUDDGPR2G1PFwUSpEEwFPDnSX",
         "orgAccount": "tz1hRTppkUow3wQNcj9nZ9s5snwc6sGC8QHh",
-        "chain": process.env.CHAIN || "florencenet",
-        "apiEndpoint": process.env.INDEXER_API_ENDPOINT || "https://api.florencenet.tzkt.io/",
-        "provider": process.env.NODE_PROVIDER || "https://florencenet.smartpy.io/"
+        "chain": process.env.NEXT_PUBLIC_CHAIN || "florencenet",
+        "apiEndpoint": process.env.NEXT_PUBLIC_INDEXER_API_ENDPOINT || "https://api.florencenet.tzkt.io/",
+        "provider": process.env.NEXT_PUBLIC_NODE_PROVIDER || "https://florencenet.smartpy.io/"
     };
 };

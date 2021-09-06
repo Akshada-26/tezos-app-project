040ff1889b8c670267f4f8b48993b483cb9193c1
Anil <anil@b9lab.com>
2021-09-06 15:58:21 +0200

    inrease gas amount for deployment

--- src/contract/deployment.sh
+++ src/contract/deployment.sh
@@ -35,6 +35,6 @@ tezos-client originate contract TZMINT transferring 0 from bob running "/opt/con
             (Pair
                 4000
                 (Pair 0 0)))))' \
- --fee 0.011926 \
+ --fee 0.1 \
  --gas-limit 42803 \
  --storage-limit 7514

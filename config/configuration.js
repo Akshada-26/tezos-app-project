76760ff4f56d5f5d760e03ed7173c81f36b8d521
Ibo Sy <ibo@b9lab.com>
2021-09-24 14:49:12 +0200

    update default config, point to granada

--- config/configuration.js
+++ config/configuration.js
@@ -6,7 +6,7 @@ exports.configuration = () => {
     return {
         "contractAddress": process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || "KT1NVM3krSTHbkQSuCcGGqXzKSePFF3LKfED",
         "orgAccount": "tz1hRTppkUow3wQNcj9nZ9s5snwc6sGC8QHh",
-        "chain": process.env.NEXT_PUBLIC_CHAIN || "florencenet",
+        "chain": process.env.NEXT_PUBLIC_CHAIN || "granadanet",
         "apiEndpoint": process.env.NEXT_PUBLIC_INDEXER_API_ENDPOINT || "https://api.granadanet.tzkt.io/",
         "provider": process.env.NEXT_PUBLIC_NODE_PROVIDER || "https://granadanet.smartpy.io/"
     };

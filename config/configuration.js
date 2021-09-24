39f47df048472f1310b609f5354ba3fd49c39bc1
Anil <anil@b9lab.com>
2021-09-24 12:56:12 +0200

    deploy contract granada

--- config/configuration.js
+++ config/configuration.js
@@ -4,10 +4,10 @@
 
 exports.configuration = () => {
     return {
-        "contractAddress": process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || "KT1U2XhTEupMdbctb2kcCzkVanHCTaWg1azi",
+        "contractAddress": process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || "KT1NVM3krSTHbkQSuCcGGqXzKSePFF3LKfED",
         "orgAccount": "tz1hRTppkUow3wQNcj9nZ9s5snwc6sGC8QHh",
         "chain": process.env.NEXT_PUBLIC_CHAIN || "florencenet",
-        "apiEndpoint": process.env.NEXT_PUBLIC_INDEXER_API_ENDPOINT || "https://api.florencenet.tzkt.io/",
-        "provider": process.env.NEXT_PUBLIC_NODE_PROVIDER || "https://florencenet.smartpy.io/"
+        "apiEndpoint": process.env.NEXT_PUBLIC_INDEXER_API_ENDPOINT || "https://api.granadanet.tzkt.io/",
+        "provider": process.env.NEXT_PUBLIC_NODE_PROVIDER || "https://granadanet.smartpy.io/"
     };
 };

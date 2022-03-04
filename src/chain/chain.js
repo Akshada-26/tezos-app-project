3f5b6ccfefe0fb19243e2b118124ad407e967eee
Anil <anil@b9lab.com>
2022-03-04 18:59:48 +0100

    fix naming

--- src/chain/chain.js
+++ src/chain/chain.js
@@ -92,9 +92,9 @@ exports.chainWrapper = (options) => {
                     throw new Error(error);
                 });
         },
-        cashedData = (cashedStorage, storageFunction, functionToCash) => {
-            if (cashedStorage) {
-                return functionToCash(cashedStorage);
+        cachedData = (cachedStorage, storageFunction, functionToCash) => {
+            if (cachedStorage) {
+                return functionToCash(cachedStorage);
             }
 
             return storageFunction().then(functionToCash);
@@ -118,68 +118,68 @@ exports.chainWrapper = (options) => {
     return {
         "storage": requestStorage,
         "totalTokens": (storageData) => {
-            return cashedData(storageData, requestStorage, (data) => {
+            return cachedData(storageData, requestStorage, (data) => {
                 return parseInt(data.total_tokens, 10);
             });
         },
         "totalInvestments": (storageData) => {
-            return cashedData(storageData, requestStorage, (data) => {
+            return cachedData(storageData, requestStorage, (data) => {
                 return parseInt(data.total_investment, 10);
             });
         },
         "mfg": (storageData) => {
-            return cashedData(storageData, requestStorage, (data) => {
+            return cachedData(storageData, requestStorage, (data) => {
                 return parseInt(data.MFG, 10);
             });
         },
         "sellSlope": (storageData) => {
-            return cashedData(storageData, requestStorage, (data) => {
+            return cachedData(storageData, requestStorage, (data) => {
                 return parseInt(data.s, 10);
             });
         },
         "buySlope": (storageData) => {
-            return cashedData(storageData, requestStorage, (data) => {
+            return cachedData(storageData, requestStorage, (data) => {
                 return parseInt(data.b, 10);
             });
         },
         "i": (storageData) => {
-            return cashedData(storageData, requestStorage, (data) => {
-                return parseInt(data.I, 10);
+            return cachedData(storageData, requestStorage, (data) => {
+                return parseInt(data.funds_ratio_for_reserve, 10);
             });
         },
         "d": (storageData) => {
-            return cashedData(storageData, requestStorage, (data) => {
-                return parseInt(data.D, 10);
+            return cachedData(storageData, requestStorage, (data) => {
+                return parseInt(data.revenues_ratio_for_reserve, 10);
             });
         },
         "unlockingDate": (storageData) => {
-            return cashedData(storageData, requestStorage, (data) => {
+            return cachedData(storageData, requestStorage, (data) => {
                 return data.MPT;
             });
         },
         "burnedTokens": (storageData) => {
-            return cashedData(storageData, requestStorage, (data) => {
+            return cachedData(storageData, requestStorage, (data) => {
                 return parseInt(data.burned_tokens, 10);
             });
         },
         "companyName": (storageData) => {
-            return cashedData(storageData, requestStorage, (data) => {
+            return cachedData(storageData, requestStorage, (data) => {
                 return data.company_name;
             });
         },
         "companyValuation": (storageData) => {
-            return cashedData(storageData, requestStorage, (data) => {
+            return cachedData(storageData, requestStorage, (data) => {
                 return parseInt(data.company_v, 10);
             });
         },
         "phase": (storageData) => {
-            return cashedData(storageData, requestStorage, (data) => {
+            return cachedData(storageData, requestStorage, (data) => {
                 return parseInt(data.phase, 10);
             });
 
         },
         "buyPrice": (storageData, tokens) => {
-            return cashedData(storageData, requestStorage, (data) => {
+            return cachedData(storageData, requestStorage, (data) => {
                 const firstPart = data.b * data.total_tokens * tokens / 2,
                     secondPart = data.b * tokens * (parseInt(data.total_tokens, 10) + tokens) / 2,
                     tempPrice = Math.ceil((firstPart + secondPart) * 10) / 10;
@@ -191,44 +191,44 @@ exports.chainWrapper = (options) => {
             });
         },
         "baseCurrency": (storageData) => {
-            return cashedData(storageData, requestStorage, (data) => {
+            return cachedData(storageData, requestStorage, (data) => {
                 return data.base_currency;
             });
         },
         "totalAllocation": (storageData) => {
-            return cashedData(storageData, requestStorage, (data) => {
+            return cachedData(storageData, requestStorage, (data) => {
                 return parseInt(data.total_allocation, 10);
             });
         },
         "stakeAllocation": (storageData) => {
-            return cashedData(storageData, requestStorage, (data) => {
+            return cachedData(storageData, requestStorage, (data) => {
                 return parseInt(data.stake_allocation, 10);
             });
         },
         "terminationEvents": (storageData) => {
-            return cashedData(storageData, requestStorage, (data) => {
+            return cachedData(storageData, requestStorage, (data) => {
                 return data.termination_events;
             });
 
         },
         "govRights": (storageData) => {
-            return cashedData(storageData, requestStorage, (data) => {
+            return cachedData(storageData, requestStorage, (data) => {
                 return data.govRights;
             });
         },
         "totalInvestors": (storageData) => {
-            return cashedData(storageData, requestStorage, (data) => {
+            return cachedData(storageData, requestStorage, (data) => {
                 return parseInt(Object.keys(data.ledger).length - 1, 10);
             });
         },
         "minimumInvestment": (storageData) => {
-            return cashedData(storageData, requestStorage, (data) => {
+            return cachedData(storageData, requestStorage, (data) => {
                 return parseInt(data.minimumInvestment, 10);
             });
 
         },
         "administrator": (storageData) => {
-            return cashedData(storageData, requestStorage, (data) => {
+            return cachedData(storageData, requestStorage, (data) => {
                 return data.organization;
             });
 
